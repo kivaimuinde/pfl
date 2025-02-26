7960cd4939ce27f8679d6582bd559a0b512638e7
kivaimuinde <muindejosphat@gmail.com>
2025-02-26 08:48:16 +0300

    renamed reset_password url to password_Reset

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from .views import users
+
+app_name='core'
+
 urlpatterns = [
+
+    path("", users.home, name="home"),
+
     # Users URLs
     path("register/", users.register_user, name="register"),
     path("login/", users.login_user, name="login"),
     path("logout/", users.logout_user, name="logout"),
-    path("reset-password/", users.reset_password, name="reset_password"),
+    path("reset-password/", users.reset_password, name="password_reset"),
     path("change-password/", users.change_password, name="change_password"),
     path("profile/", users.profile_view, name="profile"),
     path("profile/edit/", users.profile_update, name="profile_edit"),
